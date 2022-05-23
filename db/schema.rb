4dba6ab89dad34784a68ea0cba6953a6e028a280
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-23 18:12:04 -0500

    feat: Jams model views and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_224438) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_231005) do
+  create_table "jams", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jams_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_224438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jams", "users"
 end
