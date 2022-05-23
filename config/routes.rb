de97e2ab851564f040a9ecccdf193a15d905a50d
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-23 17:51:10 -0500

    feat: Devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  get "home/index"
+  get "home/faq"
+  root to: "home#index"
 end
